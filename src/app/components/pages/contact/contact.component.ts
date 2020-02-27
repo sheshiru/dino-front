161ed310aa840c88eee1710c5da771b01df84aee
Cecile Pham <sheshiru.van@gmail.com>
2020-02-27 19:01:48 +0100

    fixing navbar and reset contact form

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,32 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 // import { FormGroup } from '@angular/forms';
-import { NgForm } from '@angular/forms';
-import { MailService } from 'src/app/services/mail.service';
+import { NgForm } from "@angular/forms";
+import { MailService } from "src/app/services/mail.service";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"]
 })
 export class ContactComponent implements OnInit {
-
   name;
   email;
   text;
-  
-  constructor(private mail:MailService) { }
 
-  ngOnInit() {
+  constructor(private mail: MailService) {}
 
-  }
+  ngOnInit() {}
 
-  submit(form:NgForm){
+  submit(form: NgForm) {
     const formData = {
       name: form.value["name"],
-      email:form.value["email"],
+      email: form.value["email"],
       text: form.value["text"]
-    }
+    };
     console.log(formData);
     this.mail.sendMail(formData);
+    form.reset();
   }
 }
